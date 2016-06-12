433520aecd38f5a3a4cb6296f38efc7afc3d555a
Jona Rañola (IS-PH) <jonar@ph.trendnet.org>
2016-06-12 12:09:28 +0800

    Added UI

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
+
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
-def index():
+def index(): 
     return "Hello, World!!"
+
+@app.route('/templates/')
+def templates():
+    return render_template("index.html")
